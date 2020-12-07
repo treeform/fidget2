fb513d7479aeef6a1bd670fb2405b9484eff2b0d
treeform <starplant@gmail.com>
2020-12-07 08:49:15 -0800

    Experiments.

---
+++ experiments/run_shaders.nim
@@ -0,0 +1,197 @@
+import math, opengl, staticglfw, times, vmath, shadercompiler
+
+# init libraries
+if init() == 0:
+  raise newException(Exception, "Failed to intialize GLFW")
+
+# Turn on anti-aliasing?
+# windowHint(SAMPLES, 4)
+
+# Open a window
+var window = createWindow(
+  1000, 1000,
+  "run_shaders",
+  nil,
+  nil)
+window.makeContextCurrent()
+
+# Load opengl
+loadExtensions()
+
+
+proc basic2dVert(vertexPox: Vec2, gl_Position: var Vec4) =
+  gl_Position.xy = vertexPox
+
+# The data for (and about) OpenGL
+var
+  # Vertex data
+
+  vertices: array[8, GLfloat] = [
+    -1.float32, -1,
+    -1, +1,
+    +1, +1,
+    +1, -1,
+  ]
+
+  # Index data
+  indices: array[4, GLubyte] = [0.uint8, 1, 2, 3]
+
+  # OpenGL data
+  vertexVBO: GLuint
+  vao: GLuint
+  vertShader: GLuint
+  fragShader: GLuint
+  shaderProgram: GLuint
+
+  # Shader source
+#   vertShaderSrc = """#version 300 es
+# layout(location = 0) in vec2 vertexPos;
+# void main() {
+#   gl_Position.xy = vertexPos;
+# }
+# """
+
+  vertShaderSrc = toShader(basic2dVert, "300 es")
+  fragShaderSrc = readFile("svg2.glsl")
+
+  vertShaderArray = allocCStringArray([vertShaderSrc])  # dealloc'd at the end
+  fragShaderArray = allocCStringArray([fragShaderSrc])  # dealloc'd at the end
+
+  # Status variables
+  isCompiled: GLint
+  isLinked: GLint
+
+echo vertShaderSrc
+
+# Bind the vertices
+glGenBuffers(1, vertexVBO.addr)
+glBindBuffer(GL_ARRAY_BUFFER, vertexVBO)
+glBufferData(GL_ARRAY_BUFFER, vertices.sizeof, vertices.addr, GL_STATIC_DRAW)
+
+# The array object
+glGenVertexArrays(1, vao.addr)
+glBindVertexArray(vao)
+glBindBuffer(GL_ARRAY_BUFFER, vertexVBO);
+glVertexAttribPointer(0, 2, cGL_FLOAT, GL_FALSE, 0, nil)
+glEnableVertexAttribArray(0)
+
+# Compile shaders
+# Vertex
+vertShader = glCreateShader(GL_VERTEX_SHADER)
+glShaderSource(vertShader, 1, vertShaderArray, nil)
+glCompileShader(vertShader)
+glGetShaderiv(vertShader, GL_COMPILE_STATUS, isCompiled.addr)
+
+# Check vertex compilation status
+if isCompiled == 0:
+  echo "Vertex Shader wasn't compiled.  Reason:"
+  var logSize: GLint
+  glGetShaderiv(vertShader, GL_INFO_LOG_LENGTH, logSize.addr)
+  var
+    logStr = cast[ptr GLchar](alloc(logSize))
+    logLen: GLsizei
+  glGetShaderInfoLog(vertShader, logSize.GLsizei, logLen.addr, logStr)
+  echo $logStr
+  dealloc(logStr)
+else:
+  echo "Vertex Shader compiled successfully."
+
+# Fragment
+fragShader = glCreateShader(GL_FRAGMENT_SHADER)
+glShaderSource(fragShader, 1, fragShaderArray, nil)
+glCompileShader(fragShader)
+glGetShaderiv(fragShader, GL_COMPILE_STATUS, isCompiled.addr)
+
+# Check Fragment compilation status
+if isCompiled == 0:
+  echo "Fragment Shader wasn't compiled.  Reason:"
+  var logSize: GLint
+  glGetShaderiv(fragShader, GL_INFO_LOG_LENGTH, logSize.addr)
+  var
+    logStr = cast[ptr GLchar](alloc(logSize))
+    logLen: GLsizei
+  glGetShaderInfoLog(fragShader, logSize.GLsizei, logLen.addr, logStr)
+  echo $logStr
+  dealloc(logStr)
+else:
+  echo "Fragment Shader compiled successfully."
+
+# Attach to a GL program
+shaderProgram = glCreateProgram()
+glAttachShader(shaderProgram, vertShader);
+glAttachShader(shaderProgram, fragShader);
+
+# insert locations
+glBindAttribLocation(shaderProgram, 0, "vertexPos");
+glBindAttribLocation(shaderProgram, 0, "vertexClr");
+
+glLinkProgram(shaderProgram);
+
+# Check for shader linking errors
+glGetProgramiv(shaderProgram, GL_LINK_STATUS, isLinked.addr)
+if isLinked == 0:
+  echo "Wasn't able to link shaders.  Reason:"
+  var logSize: GLint
+  glGetProgramiv(shaderProgram, GL_INFO_LOG_LENGTH, logSize.addr)
+  var
+    logStr = cast[ptr GLchar](alloc(logSize))
+    logLen: GLsizei
+  glGetProgramInfoLog(shaderProgram, logSize.GLsizei, logLen.addr, logStr)
+  echo $logStr
+  dealloc(logStr)
+else:
+  echo "Shader Program ready!"
+
+# If everything is linked, that means we're good to go!
+if isLinked == 1:
+
+  # Some other options
+  # glEnable(GL_MULTISAMPLE)
+
+  # Record the time
+  var
+    startTs = epochTime()
+    frameCount = 0
+
+  # Main loop
+  while windowShouldClose(window) == 0:
+
+    # Exit on `ESC` press
+    if getKey(window, KEY_ESCAPE) == 1:
+      setWindowShouldClose(window, 1)
+
+    # Clear and setup drawing
+    glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
+    glUseProgram(shaderProgram)
+
+    # Do the drawing
+    glBindVertexArray(vao)
+    glDrawElements(GL_TRIANGLE_FAN, indices.len.GLsizei, GL_UNSIGNED_BYTE, indices.addr)
+
+    # Unbind
+    glBindVertexArray(0);
+    glUseProgram(0);
+
+    # Poll and swap
+    pollEvents()
+    swapBuffers(window)
+
+    inc frameCount
+    if frameCount mod 144 == 0:
+      echo 1 / ((epochTime() - startTs) / frameCount.float64), "fps"
+
+# Cleanup non-GC'd stuff
+deallocCStringArray(vertShaderArray)
+deallocCStringArray(fragShaderArray)
+
+# Cleanup OpenGL Stuff
+glDeleteProgram(shaderProgram)
+glDeleteShader(vertShader)
+glDeleteShader(fragShader)
+glDeleteBuffers(1, vertexVBO.addr)
+#glDeleteBuffers(1, colorVBO.addr)
+glDeleteVertexArrays(1, vao.addr)
+
+# cleanup GLFW
+destroyWindow(window)
+terminate()
---
+++ experiments/shadercompiler.nim
@@ -0,0 +1,225 @@
+## Shader macro, converts nim code into GLSL
+
+import chroma, macros, strutils, vmath
+
+type
+  uniform*[T] = T
+
+proc typeRename(t: string): string =
+  ## Some GLSL type names don't match nim names, rename here.
+  case t
+  of "Mat4": "mat4"
+  of "Mat3": "mat3"
+  of "Color": "vec4"
+  of "Vec4": "vec4"
+  of "Vec3": "vec3"
+  of "Vec2": "vec2"
+  of "float32": "float"
+  else: t
+
+proc procRename(t: string): string =
+  ## Some GLSL proc names don't match nim names, rename here.
+  case t
+  of "color": "vec4"
+  else: t
+
+proc addIndent(res: var string, level: int) =
+  for i in 0 ..< level:
+    res.add "  "
+
+proc toCodeStmts(n: NimNode, res: var string, level = 0)
+
+proc toCode(n: NimNode, res: var string, level = 0) =
+  ## Inner code block.
+  if n.kind == nnkEmpty:
+    return
+  case n.kind
+  of nnkAsgn:
+    res.addIndent level
+    n[0].toCode(res)
+    res.add " = "
+    n[1].toCode(res)
+  of nnkInfix:
+    n[1].toCode(res)
+    res.add " "
+    n[0].toCode(res)
+    res.add " "
+    n[2].toCode(res)
+  of nnkHiddenDeref, nnkHiddenAddr:
+    n[0].toCode(res)
+  of nnkCall:
+    var procName = procRename(n[0].strVal)
+    if procName in ["rgb=", "rgb", "xyz", "xy", "xy="]:
+      if n[1].kind == nnkSym:
+        n[1].toCode(res)
+      else:
+        res.add "("
+        n[1].toCode(res)
+        res.add ")"
+      res.add "."
+      res.add procName.replace("=", " = ")
+      if n.len == 3:
+        n[2].toCode(res)
+    else:
+      res.add procName
+      res.add "("
+      for j in 1 ..< n.len:
+        if j != 1: res.add ", "
+        n[j].toCode(res)
+      res.add ")"
+  of nnkDotExpr:
+    n[0].toCode(res)
+    res.add "."
+    n[1].toCode(res)
+  of nnkIdent, nnkSym:
+    res.add n.strVal
+  of nnkStmtListExpr:
+    for j in 0 ..< n.len:
+      n[j].toCode(res, level)
+  of nnkStmtList:
+    for j in 0 ..< n.len:
+      if n[j].kind in [nnkCall]:
+        res.addIndent level
+      n[j].toCode(res, level)
+      if n[j].kind notin [nnkLetSection, nnkVarSection]:
+        res.add ";\n"
+  of nnkIfStmt:
+    res.addIndent level
+    res.add "if ("
+    n[0][0].toCode(res)
+    res.add ") {\n"
+    n[0][1].toCodeStmts(res, level + 1)
+    res.addIndent level
+    res.add "}"
+    if n.len > 1:
+      # TODO else if?
+      if n[1].kind == nnkElse:
+        res.add " else {\n"
+        n[1][0].toCodeStmts(res, level + 1)
+        res.addIndent level
+        res.add "}"
+  of nnkHiddenStdConv:
+    for j in 0 .. n.len-1:
+      n[j].toCode(res)
+  of nnkEmpty, nnkNilLit:
+    discard # same as nil node in this representation
+  of nnkCharLit .. nnkInt64Lit:
+    res.add $n.intVal
+  of nnkFloatLit .. nnkFloat64Lit:
+    res.add $n.floatVal
+  of nnkStrLit .. nnkTripleStrLit, nnkCommentStmt:
+    res.add $n.strVal.newLit.repr
+  of nnkNone:
+    assert false
+  of nnkVarSection, nnkLetSection:
+    for j in 0 ..< n.len:
+      res.addIndent level
+      n[j].toCode(res, level)
+      res.add ";\n"
+  of nnkIdentDefs:
+    for j in countup(0, n.len - 1, 3):
+      res.add typeRename(n[j + 2].getTypeInst().strVal)
+      res.add " "
+      n[j].toCode(res)
+      res.add " = "
+      n[j + 2].toCode(res)
+  else:
+    res.add ($n.kind)
+    res.add "{{"
+    for j in 0 .. n.len-1:
+      n[j].toCode(res)
+    res.add "}}"
+
+proc toCodeStmts(n: NimNode, res: var string, level = 0) =
+  if n.kind != nnkStmtList:
+    res.addIndent level
+    n.toCode(res, level)
+    res.add ";\n"
+  else:
+    n.toCode(res, level)
+
+proc toCodeTopLevel(topLevelNode: NimNode, res: var string, level = 0) =
+  ## Top level block such as in and out params.
+  assert topLevelNode.kind == nnkProcDef
+  for n in topLevelNode:
+    case n.kind
+    of nnkEmpty:
+      discard
+    of nnkSym:
+      res.add "// name: "
+      res.add $n
+      res.add "\n\n"
+    of nnkFormalParams:
+      for param in n:
+        if param.kind != nnkEmpty:
+          if param[1].kind == nnkVarTy:
+            if param[1][0].strVal == "int":
+              res.add "flat "
+            res.add "out "
+            res.add typeRename(param[1][0].strVal)
+          else:
+            if param[1].kind == nnkBracketExpr:
+              res.add param[1][0].strVal
+              res.add " "
+              res.add typeRename(param[1][1].strVal)
+            else:
+              if param[1].strVal == "int":
+                res.add "flat "
+              res.add "in "
+              res.add typeRename(param[1].strVal)
+          res.add " "
+          res.add param[0].strVal
+          res.add ";\n"
+    else:
+      res.add "\n"
+      res.add "void main() {\n"
+      n.toCodeStmts(res, level+1)
+      res.add "}"
+
+macro toShader*(s: typed, version = "410"): string =
+  ## Converts proc to a glsl string.
+  var code: string
+  code.add "#version " & version.strVal & "\n\n"
+  getImpl(s).toCodeTopLevel(code)
+  result = newLit(code)
+
+type
+  Color* = object
+    ## Main color type, float32 points
+    r*: float32 ## red (0-1)
+    g*: float32 ## green (0-1)
+    b*: float32 ## blue (0-1)
+    a*: float32 ## alpha (0-1, 0 is fully transparent)
+
+proc color*(r, g, b: float32, a: float32 = 1.0): Color {.inline.} =
+  ## Creates from floats like:
+  ## * color(1,0,0) -> red
+  ## * color(0,1,0) -> green
+  ## * color(0,0,1) -> blue
+  ## * color(0,0,0,1) -> opaque  black
+  ## * color(0,0,0,0) -> transparent black
+  Color(r: r, g: g, b: b, a: a)
+
+proc rgb*(c: Color): Vec3 =
+  vec3(c.r, c.g, c.b)
+
+proc `rgb=`*(c: var Color, v: Vec3) =
+  c.r = v.x
+  c.g = v.y
+  c.b = v.z
+
+proc vec4*(v: Vec3, w: float32): Vec4 =
+  vec4(v.x, v.y, v.z, w)
+
+# proc xyz*(v: Vec4): Vec3 =
+#   vec3(v.x, v.y, v.z)
+
+proc mix*(a, b: Vec3, v: float32): Vec3 =
+  lerp(a, b, v)
+
+proc `*`*(m: Mat4, v: Vec4): Vec4 =
+  vec4(m * v.xyz, 1.0)
+
+proc `xy=`*(a: var Vec4, b: Vec2) =
+  a.x = b.x
+  a.y = b.y
