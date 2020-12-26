1f8742ce1cf28914fee812d420b151d891169b4a
treeform <starplant@gmail.com>
2020-12-25 16:30:01 -0800

    Fix layouts example.

--- examples/layouts/layouts.nim
+++ examples/layouts/layouts.nim
@@ -2,11 +2,8 @@ import fidget2, bumpy
 
 use("https://www.figma.com/file/fn8PBPmPXOHAVn3N5TSTGs")
 
-onDisplay "/Layout1":
-  thisNode.box.wh = windowSize
-
 startFidget(
   windowTitle = "Layouts",
-  entryFrame = "Layout1",
+  entryFrame = "Layout4",
   resizable = true
 )
