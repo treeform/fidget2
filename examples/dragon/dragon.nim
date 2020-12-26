826e8a4e445959209ad0f45bbed1dfef9b3336f6
treeform <starplant@gmail.com>
2020-12-25 16:29:37 -0800

    Fix Dragon example

--- examples/dragon/dragon.nim
+++ examples/dragon/dragon.nim
@@ -3,6 +3,12 @@ import fidget2
 # Connect a figma file to the code base.
 use("https://www.figma.com/file/Cto22A31tUso9On23AIpM7")
 
-# mainFrame = "Crew Dragon Flight Control UI"
+# mainFrame =
 # windowSizeFixed = true
 # startFidget()
+
+startFidget(
+  windowTitle = "Crew Dragon Flight Control UI",
+  entryFrame = "Crew Dragon Flight Control UI",
+  resizable = false
+)
