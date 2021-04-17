dd0703b9843de3ad22ec4e8d5bf94731d004d5f4
treeform <starplant@gmail.com>
2021-04-17 10:55:52 -0700

    changes to dragon sample

--- examples/dragon/dragon.nim
+++ examples/dragon/dragon.nim
@@ -1,8 +1,8 @@
 import fidget2
 
 startFidget(
-  figmaUrl = "https://www.figma.com/file/Cto22A31tUso9On23AIpM7",
+  figmaUrl = "https://www.figma.com/file/TQOSRucXGFQpuOpyTkDYj1",
   windowTitle = "Crew Dragon Flight Control UI",
-  entryFrame = "Crew Dragon Flight Control UI",
+  entryFrame = "dragon",
   resizable = false
 )
