d9f9af9082171c2496a2bf98971fcb0a3ab1213e
treeform <starplant@gmail.com>
2021-12-30 15:14:00 -0800

    Add inspector.

---
+++ examples/inspector/config.nims
@@ -0,0 +1,4 @@
+--gc:arc
+--threads:on
+--deepcopy:on
+--tlsEmulation:off
---
+++ examples/inspector/inspector.nim
@@ -0,0 +1,8 @@
+import fidget2
+
+startFidget(
+  figmaUrl = "https://www.figma.com/file/FYpiqnazfZ2Qk4oJi9sLwa/Figma-Inspector",
+  windowTitle = "Inspector",
+  entryFrame = "Main/InspectorWhite",
+  resizable = true
+)
